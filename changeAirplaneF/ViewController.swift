33fda86142a212a02ca17f32b3c853cdb44c3822
Fausto Checa <fausto.checa@gmail.com>
2017-06-16 23:43:26 +0200

    Initial Commit

---
+++ changeAirplaneF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  changeAirplaneF
+//
+//  Created by Fausto Checa on 16/6/17.
+//  Copyright © 2017 Fausto Checa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
